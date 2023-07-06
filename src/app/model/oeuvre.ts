0020960da4ea97022e0b9dd90c061514e454c831
Daenrya <marjolaine.arb@gmail.com>
2023-07-06 09:00:00 +0200

    creation classe Oeuvre

---
+++ src/app/model/oeuvre.ts
@@ -0,0 +1,11 @@
+export class Oeuvre {
+    idOeuvre: number;
+    nomOeuvre: string;
+    dureeOeuvre: number;
+  
+    constructor(idOeuvre: number, nomOeuvre: string, dureeOeuvre: number) {
+      this.idOeuvre = idOeuvre;
+      this.nomOeuvre = nomOeuvre;
+      this.dureeOeuvre = dureeOeuvre;
+    }
+}
