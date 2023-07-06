11aee0114b21806a8555d98bd4c537ae59ae2cdb
Pierrot82 <pierre.larroque.82@hotmail.fr>
2023-07-06 10:15:28 +0200

    ajout des routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AjouterOeuvreComponent } from './ajouter-oeuvre/ajouter-oeuvre.component';
+import { ChefOrchestreComponent } from './chefOrchestre/chef-orchestre/chef-orchestre.component';
+import { SolisteComponent } from './soliste/soliste.component';
+import { UpdateSolisteComponent } from './update-soliste/update-soliste.component';
+import { FormulaireSolisteComponent } from './formulaire-soliste/formulaire-soliste.component';
 
 const routes: Routes = [
   {path:"ajoutOeuvre", component:AjouterOeuvreComponent},
+  {path:"listeChefOrchestre", component:ChefOrchestreComponent},
+  {path:"soliste", component:SolisteComponent},
+  {path:"formulaireSoliste", component:FormulaireSolisteComponent}
 ];
 
 @NgModule({
